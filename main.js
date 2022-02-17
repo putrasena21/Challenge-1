43d885495551ad9f7bef10e56817a396984196ec
mochammad faisal <bullenxz12@gmail.com>
2022-02-18 02:13:37 +0700

    main.js v1.0

---
+++ main.js
@@ -0,0 +1,14 @@
+function luasPersegi(sisi) {
+    let luas = sisi * sisi
+    return luas    
+}
+
+function volumeKubus(sisi) {
+    let result = sisi * sisi * sisi
+    return result
+}
+
+function volumeTabung(rusuk, tinggi) {
+    let result = Math.PI * rusuk * rusuk * tinggi
+    return result
+}
