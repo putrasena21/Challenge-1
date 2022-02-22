6adadf9488d115ebcff5eb7d8c88ddf33b948d24
mochammad faisal <bullenxz12@gmail.com>
2022-02-22 19:09:54 +0700

    removing ignored files

--- usePrompt.js
+++ usePrompt.js
@@ -3,6 +3,6 @@ const prompt = require('prompt');
 prompt.start();
 prompt.get(['username', 'email'], (err,result)=>{
     console.log(`Command-line input received: `)
-    console.log(`username: ${result.username}`)
+    console.log(`username: ${result.username}`);
     console.log(`email : ${result.email}`);
 })
